fc320bf91976c065b4e76e9a3577bf8ab0c9f7f0
Omar Yasser <oyaraouf@gmail.com>
2020-03-23 21:33:52 +0100

    Generate Starter App using start.spring.io

---
+++ src/main/java/com/omar/azkar/AzkarApplication.java
@@ -0,0 +1,13 @@
+package com.omar.azkar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzkarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzkarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/omar/azkar/AzkarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omar.azkar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzkarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
