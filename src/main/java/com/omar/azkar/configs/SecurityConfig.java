0c7eb272225a0ef3268b4534c0879208609a4666
Omar Yasser <oyaraouf@gmail.com>
2020-04-01 01:45:51 +0200

    Remove ignoring security property

--- src/main/java/com/omar/azkar/configs/SecurityConfig.java
+++ src/main/java/com/omar/azkar/configs/SecurityConfig.java
@@ -9,34 +9,34 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
-
-@Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    http.authorizeRequests()
-        .anyRequest()
-        .authenticated()
-        .and()
-        .oauth2Login()
-        .defaultSuccessUrl("/loginSuccess", true)
-        .failureUrl("/loginFailure");
-
-    // allowing cors origin.
-    http.cors().and().csrf().disable();
-
-  }
-
-  @Bean
-  CorsConfigurationSource corsConfigurationSource() {
-    CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(Arrays.asList("*"));
-    configuration.setAllowedMethods(Arrays.asList("*"));
-    configuration.setAllowedHeaders(Arrays.asList("*"));
-    configuration.setAllowCredentials(true);
-    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    source.registerCorsConfiguration("/**", configuration);
-    return source;
-  }
-}
+//
+//@Configuration
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//  @Override
+//  protected void configure(HttpSecurity http) throws Exception {
+//    http.authorizeRequests()
+//        .anyRequest()
+//        .authenticated()
+//        .and()
+//        .oauth2Login()
+//        .defaultSuccessUrl("/loginSuccess", true)
+//        .failureUrl("/loginFailure");
+//
+//    // allowing cors origin.
+//    http.cors().and().csrf().disable();
+//
+//  }
+//
+//  @Bean
+//  CorsConfigurationSource corsConfigurationSource() {
+//    CorsConfiguration configuration = new CorsConfiguration();
+//    configuration.setAllowedOrigins(Arrays.asList("*"));
+//    configuration.setAllowedMethods(Arrays.asList("*"));
+//    configuration.setAllowedHeaders(Arrays.asList("*"));
+//    configuration.setAllowCredentials(true);
+//    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//    source.registerCorsConfiguration("/**", configuration);
+//    return source;
+//  }
+//}
