01150dbc2bde8e20c1dd6a72da3c0fcc71a9af91
Ahmed <aabdelzaher96@gmail.com>
2020-04-02 14:47:53 +0200

    Adding JWT secret.
    Adding JWT secret.
    
    Removing unrelated files
    
    Removing irrelevant file.

--- src/main/java/com/omar/azkar/configs/MongoConfig.java
+++
@@ -1,9 +0,0 @@
-package com.omar.azkar.configs;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
-
-@Configuration
-@EnableMongoAuditing
-public class MongoConfig {
-}
