9a5d9f6b151260649d2832c8de7aeb61b7832b04
Omar Yasser <oyaraouf@gmail.com>
2020-04-03 23:48:22 +0200

    Add style check plugin and resolve all check errors (#18)
    The plugin added is maven-checkstyle-plugin. I configured this plugin
    to use google-style-check.xml file. I removed java doc requirements
    from google-style-check.xml. Also as a preparation for Continuous
    integration, I set the the violation sevirty to "warning". Which means
    any warning from the style checks will make the build fail.
    
    To check the violations run: mvnw checksyle:check
    If any warnings/errors are there, mvnw verify will fail.

--- src/main/java/com/omar/azkar/repos/UserRepo.java
+++ src/main/java/com/omar/azkar/repos/UserRepo.java
@@ -1,10 +1,9 @@
 package com.omar.azkar.repos;
 
 import com.omar.azkar.entities.User;
+import java.util.Optional;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 @Repository
 public interface UserRepo extends MongoRepository<User, String> {
--- src/main/java/com/omar/azkar/services/UserService.java
+++ src/main/java/com/omar/azkar/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
   @Autowired
   private UserRepo userRepository;
 
