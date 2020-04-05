6620d048f3997245e3c5d48f0535c961f4cbc261
Ahmed <aabdelzaher96@gmail.com>
2020-04-05 16:26:18 +0200

    Reformatting

--- src/main/java/com/azkar/controllers/GroupController.java
+++ src/main/java/com/azkar/controllers/GroupController.java
@@ -11,7 +11,6 @@ import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/com/azkar/entities/Group.java
+++ src/main/java/com/azkar/entities/Group.java
@@ -1,6 +1,8 @@
 package com.azkar.entities;
 
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import lombok.Builder;
 import lombok.Builder.Default;
 import lombok.Getter;
@@ -11,31 +13,19 @@ import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.Date;
-import java.util.List;
-
 @Document(collection = "groups")
 @Builder
 @Getter
 public class Group {
 
-  @Id
-  private String id;
-  @NonNull
-  private final String name;
-  @NonNull
-  private final GroupCardinality cardinality;
-  @NonNull
-  private final String adminId;
-  @NonNull
-  private List<String> usersIds;
-  @DBRef
-  @Default
-  private List<Challenge> challenges = new ArrayList<>();
-  @CreatedDate
-  private Date createdAt;
-  @LastModifiedDate
-  private Date modifiedAt;
+  @Id private String id;
+  @NonNull private final String name;
+  @NonNull private final GroupCardinality cardinality;
+  @NonNull private final String adminId;
+  @NonNull private List<String> usersIds;
+  @DBRef @Default private List<Challenge> challenges = new ArrayList<>();
+  @CreatedDate private Date createdAt;
+  @LastModifiedDate private Date modifiedAt;
 
   public enum GroupCardinality {
     SINGLE,
