47a927349cf015e22f0427f5b0281fa847a8c523
Omar Yasser <oyaraouf@gmail.com>
2020-04-10 22:44:08 +0200

    Format

--- src/main/java/com/azkar/controllers/GroupController.java
+++ src/main/java/com/azkar/controllers/GroupController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class GroupController extends BaseController tens {
+public class GroupController extends BaseController {
 
   @Autowired
   private GroupRepo groupRepo;
