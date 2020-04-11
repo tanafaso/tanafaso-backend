56cffa309841e96e37d92da026d45848c7a5b960
Omar Yasser <oyaraouf@gmail.com>
2020-04-11 12:38:31 +0200

    Refine content type expectations (#38)
    * Refine content type expectations

--- src/main/java/com/azkar/controllers/HomeController.java
+++ src/main/java/com/azkar/controllers/HomeController.java
@@ -1,5 +1,6 @@
 package com.azkar.controllers;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController extends BaseController {
 
-  @GetMapping(value = "/", produces = "application/json")
+  @GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
   ResponseEntity home() {
     return ResponseEntity.ok().build();
   }
--- src/main/java/com/azkar/controllers/UserController.java
+++ src/main/java/com/azkar/controllers/UserController.java
@@ -7,27 +7,30 @@ import com.azkar.payload.usercontroller.GetUsersResponse;
 import com.azkar.repos.UserRepo;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 public class UserController extends BaseController {
 
   @Autowired
   private UserRepo userRepo;
 
-  @GetMapping(path = "/users", produces = "application/json")
+  @GetMapping(path = "/users")
   public ResponseEntity<GetUsersResponse> getUsers() {
     GetUsersResponse response = new GetUsersResponse();
     response.setData(userRepo.findAll());
     return ResponseEntity.ok(response);
   }
 
-  @GetMapping(path = "/user/{id}", produces = "application/json")
+  @GetMapping(path = "/user/{id}")
   public ResponseEntity<GetUserResponse> getUser(@PathVariable String id) {
     Optional<User> user = userRepo.findById(id);
     if (!user.isPresent()) {
@@ -38,7 +41,7 @@ public class UserController extends BaseController {
     return ResponseEntity.ok(response);
   }
 
-  @PostMapping(path = "/user", consumes = "application/json", produces = "application/json")
+  @PostMapping(path = "/user", consumes = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<AddUserResponse> addUser(@RequestBody User user) {
     User newUser = User.builder().name(user.getName()).email(user.getEmail()).build();
     userRepo.save(newUser);
