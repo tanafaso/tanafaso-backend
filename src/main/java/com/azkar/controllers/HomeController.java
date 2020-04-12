86ffbfd3bdc8a150988087006fc20780e0c70883
Omar Yasser <oyaraouf@gmail.com>
2020-04-12 02:39:07 +0200

    Format

--- src/main/java/com/azkar/controllers/HomeController.java
+++ src/main/java/com/azkar/controllers/HomeController.java
@@ -3,7 +3,6 @@ package com.azkar.controllers;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController()
