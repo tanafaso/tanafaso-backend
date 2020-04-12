4512e891353dfdb8aa3f55c9bcb5463c2f2f37bd
Omar Yasser <oyaraouf@gmail.com>
2020-04-12 02:43:39 +0200

    Remove unneeded change

--- src/main/java/com/azkar/controllers/HomeController.java
+++ src/main/java/com/azkar/controllers/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController()
+@RestController
 public class HomeController extends BaseController {
 
   @GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
