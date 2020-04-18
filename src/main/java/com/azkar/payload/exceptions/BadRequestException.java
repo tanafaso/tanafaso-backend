28a7e896abd92ae2d64a952420dd42321d6e7074
Ahmed <aabdelzaher96@gmail.com>
2020-04-18 15:45:43 +0200

    Breaking 100+ charachter line into two lines

--- src/main/java/com/azkar/payload/exceptions/BadRequestException.java
+++ src/main/java/com/azkar/payload/exceptions/BadRequestException.java
@@ -2,7 +2,8 @@ package com.azkar.payload.exceptions;
 
 public class BadRequestException extends RuntimeException {
 
-  public static final String REQUIRED_FIELDS_NOT_GIVEN_ERROR = "Some required fields are not provided.";
+  public static final String REQUIRED_FIELDS_NOT_GIVEN_ERROR =
+      "Some required fields are not provided.";
 
   public BadRequestException(String message) {
     super(message);
