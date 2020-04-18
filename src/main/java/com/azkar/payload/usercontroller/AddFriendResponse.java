fcbdc62e4de8e402e4d0df08d20980c791d79ea6
Omar Yasser <oyaraouf@gmail.com>
2020-04-18 18:20:22 +0200

    Remove Friend parameter for AddFriendResponse

--- src/main/java/com/azkar/payload/usercontroller/AddFriendResponse.java
+++ src/main/java/com/azkar/payload/usercontroller/AddFriendResponse.java
@@ -1,9 +1,8 @@
 package com.azkar.payload.usercontroller;
 
-import com.azkar.entities.Friendship;
 import com.azkar.payload.ResponseBase;
 
-public class AddFriendResponse extends ResponseBase<Friendship> {
+public class AddFriendResponse extends ResponseBase {
 
   public static final String USER_NOT_FOUND_ERROR = "User not found.";
   public static final String FRIENDSHIP_ALREADY_REQUESTED_ERROR =
