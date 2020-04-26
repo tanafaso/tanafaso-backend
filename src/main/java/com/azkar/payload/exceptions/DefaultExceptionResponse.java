4c0ac1d46151c54a97dc70e8dd607ded8749c8e3
Omar Yasser <oyaraouf@gmail.com>
2020-04-26 02:47:10 +0200

    Add default exception handling

---
+++ src/main/java/com/azkar/payload/exceptions/DefaultExceptionResponse.java
@@ -0,0 +1,8 @@
+package com.azkar.payload.exceptions;
+
+import com.azkar.payload.ResponseBase;
+
+public class DefaultExceptionResponse extends ResponseBase {
+
+  public static final String DEFAULT_ERROR = "Oops, something went wrong";
+}
