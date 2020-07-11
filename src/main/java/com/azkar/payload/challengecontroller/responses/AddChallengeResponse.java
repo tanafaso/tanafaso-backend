a9f5aa2bfd498f43d73f310ec58697354f696f8b
Ahmed <aabdelzaher96@gmail.com>
2020-07-11 21:57:49 +0200

    Preventing non group members from adding challenges to the group

--- src/main/java/com/azkar/payload/challengecontroller/responses/AddChallengeResponse.java
+++ src/main/java/com/azkar/payload/challengecontroller/responses/AddChallengeResponse.java
@@ -5,4 +5,7 @@ import com.azkar.payload.ResponseBase;
 
 public class AddChallengeResponse extends ResponseBase<Challenge> {
 
+  public static final String GROUP_NOT_FOUND_ERROR = "The given group is not found.";
+  public static final String NOT_GROUP_MEMBER_ERROR =
+      "The user is not a member of the given group.";
 }
