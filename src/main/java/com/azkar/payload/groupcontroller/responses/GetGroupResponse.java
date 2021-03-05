2fe56a85b895e1128ebf87bbcde03b5e204eae0d
Omar Yasser <oyaraouf@gmail.com>
2021-03-05 09:22:35 +0100

    Fix create user after logging in with facebook

--- src/main/java/com/azkar/payload/groupcontroller/responses/GetGroupResponse.java
+++ src/main/java/com/azkar/payload/groupcontroller/responses/GetGroupResponse.java
@@ -1,9 +1,7 @@
 package com.azkar.payload.groupcontroller.responses;
 
 import com.azkar.entities.Group;
-import com.azkar.entities.User.UserGroup;
 import com.azkar.payload.ResponseBase;
-import java.util.List;
 
 public class GetGroupResponse extends ResponseBase<Group> {
 
