44ef8808e158edadba02084d953a119a2b31027f
Omar Yasser <oyaraouf@gmail.com>
2021-04-12 08:45:02 +0200

    Reformat

--- src/test/java/com/azkar/controllers/AzkarControllerTest.java
+++ src/test/java/com/azkar/controllers/AzkarControllerTest.java
@@ -3,7 +3,6 @@ package com.azkar.controllers;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.greaterThanOrEqualTo;
-import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
