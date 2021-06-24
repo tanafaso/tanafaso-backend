6f930047d228a02e0ec9a1db351739cf59bc9ad7
Omar Yasser <oyaraouf@gmail.com>
2021-06-24 09:41:15 +0200

    Filter sabeq out (#310)
    * [Saqeb] Return him only for frontend versions using the new API version
    
    * Format

--- src/main/java/com/azkar/controllers/ChallengeController.java
+++ src/main/java/com/azkar/controllers/ChallengeController.java
@@ -18,6 +18,8 @@ import com.azkar.payload.challengecontroller.responses.GetChallengeResponse;
 import com.azkar.payload.challengecontroller.responses.GetChallengesResponse;
 import com.azkar.payload.challengecontroller.responses.UpdateChallengeResponse;
 import com.azkar.payload.exceptions.BadRequestException;
+import com.azkar.payload.utils.FeaturesVersions;
+import com.azkar.payload.utils.VersionComparator;
 import com.azkar.repos.ChallengeRepo;
 import com.azkar.repos.FriendshipRepo;
 import com.azkar.repos.GroupRepo;
@@ -460,10 +462,26 @@ public class ChallengeController extends BaseController {
     if (apiVersion != null) {
       logger.info("API version requested is " + apiVersion);
     }
+    List<Challenge> userChallenges = getCurrentUser(userRepo).getUserChallenges();
+    if (VersionComparator.compare(apiVersion, FeaturesVersions.SABEQ_ADDITION_VERSION) < 0) {
+      userChallenges = filterOutSabeqChallenges(userChallenges);
+    }
+
     GetChallengesResponse response = new GetChallengesResponse();
-    response.setData(getCurrentUser(userRepo).getUserChallenges());
+    response.setData(userChallenges);
     Collections.reverse(response.getData());
     return ResponseEntity.ok(response);
+  }
+
+  private List<Challenge> filterOutSabeqChallenges(List<Challenge> challenges) {
+    List<Challenge> filtered = challenges.stream().filter(challenge -> {
+      Optional<Group> group = groupRepo.findById(challenge.getGroupId());
+      if (!group.isPresent()) {
+        return false;
+      }
+      return !group.get().getUsersIds().contains(User.SABEQ_ID);
+    }).collect(Collectors.toList());
+    return filtered;
   }
 
   @GetMapping(path = "/groups/{groupId}/")
