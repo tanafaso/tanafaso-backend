3f7393edbb1644edbdd5946f5c18145e48def7b4
Omar Yasser <oyaraouf@gmail.com>
2021-09-18 16:37:16 +0200

    Add simple home page

--- src/main/java/com/azkar/controllers/HomeController.java
+++ src/main/java/com/azkar/controllers/HomeController.java
@@ -3,20 +3,19 @@ package com.azkar.controllers;
 import com.azkar.repos.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class HomeController extends BaseController {
+
+  public static final String HOME_PAGE_VIEW_NAME = "home";
 
   @Autowired
   public UserRepo userRepo;
 
   @GetMapping(value = "/")
-  public RedirectView getHome(RedirectAttributes attributes) {
-    attributes.addFlashAttribute("flashAttribute", "redirectWithRedirectView");
-    attributes.addAttribute("attribute", "redirectWithRedirectView");
-    return new RedirectView("https://play.google.com/store/apps/details?id=com.tanafaso.azkar");
+  public String homePage(Model model) {
+    return HOME_PAGE_VIEW_NAME;
   }
 }
