65c392f49051396200fade15c7ba3fc999f58001
Omar Yasser <oyaraouf@gmail.com>
2022-04-26 22:50:00 +0200

    Clean old challenges periodically (#394)

---
+++ src/main/java/com/azkar/crons/ChallengesCleaner.java
@@ -0,0 +1,53 @@
+package com.azkar.crons;
+
+import com.azkar.entities.User;
+import com.azkar.repos.UserRepo;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@Lazy(value = false)
+public class ChallengesCleaner {
+
+  private static final Logger logger = LoggerFactory.getLogger(ChallengesCleaner.class);
+
+  @Autowired
+  private UserRepo userRepo;
+
+  // Runs once every night to delete old challenges. "Old" is a bit misleading here as the
+  // function will actually delete the oldest challenges for every user until every user has a
+  // maximum of 30 challenges per category.
+  @Scheduled(cron = "@midnight")
+  private void cleanOldChallenges() {
+    logger.info("Challenges cleaner started!");
+
+    List<User> modifiedUsers = userRepo.findAll().stream().map(user -> {
+      user.setAzkarChallenges(user.getAzkarChallenges().subList(
+          Math.max(0, user.getAzkarChallenges().size() - 30),
+          user.getAzkarChallenges().size()));
+
+      user.setReadingQuranChallenges(user.getReadingQuranChallenges().subList(
+          Math.max(0, user.getReadingQuranChallenges().size() - 30),
+          user.getReadingQuranChallenges().size()));
+
+      user.setMeaningChallenges(user.getMeaningChallenges().subList(
+          Math.max(0, user.getMeaningChallenges().size() - 30),
+          user.getMeaningChallenges().size()));
+
+      user.setMemorizationChallenges(user.getMemorizationChallenges().subList(
+          Math.max(0, user.getMemorizationChallenges().size() - 30),
+          user.getMemorizationChallenges().size()));
+      return user;
+    }).collect(Collectors.toList());
+
+    userRepo.saveAll(modifiedUsers);
+
+    logger.info("Challenges cleaner finished!");
+  }
+}
