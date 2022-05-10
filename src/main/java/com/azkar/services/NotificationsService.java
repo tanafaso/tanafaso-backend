6656c9a232239b233f055b14848388c137ed39e2
Omar Yasser <oyaraouf@gmail.com>
2022-05-10 20:57:55 +0200

    Clear obsolete notification tokens (#400)
    * Clear obsolete notification tokens

--- src/main/java/com/azkar/services/NotificationsService.java
+++ src/main/java/com/azkar/services/NotificationsService.java
@@ -1,6 +1,7 @@
 package com.azkar.services;
 
 import com.azkar.entities.User;
+import com.azkar.repos.UserRepo;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
@@ -9,17 +10,22 @@ import com.google.firebase.messaging.Aps;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.MessagingErrorCode;
 import com.google.firebase.messaging.Notification;
 import java.io.IOException;
 import javax.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NotificationsService {
 
   private static final Logger logger = LoggerFactory.getLogger(NotificationsService.class);
+
+  @Autowired
+  private UserRepo userRepo;
 
   @PostConstruct
   public void initialize() {
@@ -36,7 +42,7 @@ public class NotificationsService {
 
   public void sendNotificationToUser(User user, String title, String body) {
     if (user.getNotificationsToken() == null || user.getNotificationsToken().isEmpty()) {
-      logger.error(String.format("Token not found for user: %s", user.getId()));
+      logger.warn(String.format("Token not found for user: %s", user.getId()));
       return;
     }
 
@@ -58,8 +64,14 @@ public class NotificationsService {
       FirebaseMessaging.getInstance().send(message);
     } catch (FirebaseMessagingException e) {
       logger.warn(
-          String.format("Failed to send a notification to user: %s with title: %s, and body: %s",
-              user.getId(), title, body), e);
+          String.format("Failed to send a notification to user: %s with error code: %s, with "
+                  + "title: %s, and body: %s",
+              user.getId(), e.getMessagingErrorCode(), title, body), e);
+
+      if (e.getMessagingErrorCode() == MessagingErrorCode.UNREGISTERED) {
+        user.setNotificationsToken(null);
+        userRepo.save(user);
+      }
     }
   }
 }
