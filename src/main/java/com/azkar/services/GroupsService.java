4461c627c76d5dee05dda12126981157d536d029
Omar Yasser <oyaraouf@gmail.com>
2022-05-11 00:14:47 +0200

    Optimize get groups request (#408)

--- src/main/java/com/azkar/services/GroupsService.java
+++ src/main/java/com/azkar/services/GroupsService.java
@@ -3,6 +3,7 @@ package com.azkar.services;
 import com.azkar.entities.Group;
 import com.azkar.entities.User;
 import com.azkar.repos.GroupRepo;
+import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,8 +16,29 @@ public class GroupsService {
   GroupRepo groupRepo;
 
   public List<Group> getGroups(User user) {
-    return groupRepo.findAll().stream()
-        .filter(group -> group.getUsersIds().contains(user.getId()))
+    // Group IDs in use are the groups in which one of the user challenges belong to. Note that
+    // old challenges are deleted periodically.
+    HashSet<String> groupIdsInUse = new HashSet<>();
+    groupIdsInUse.addAll(
+        user.getAzkarChallenges().stream().map(azkarChallenge -> azkarChallenge.getGroupId())
+            .collect(
+                Collectors.toList()));
+    groupIdsInUse.addAll(
+        user.getReadingQuranChallenges().stream()
+            .map(readingQuranChallenge -> readingQuranChallenge.getGroupId())
+            .collect(
+                Collectors.toList()));
+    groupIdsInUse.addAll(
+        user.getMeaningChallenges().stream().map(meaningChallenge -> meaningChallenge.getGroupId())
+            .collect(
+                Collectors.toList()));
+    groupIdsInUse.addAll(
+        user.getMemorizationChallenges().stream()
+            .map(memorizationChallenge -> memorizationChallenge.getGroupId())
+            .collect(
+                Collectors.toList()));
+
+    return groupIdsInUse.stream().map(groupId -> groupRepo.findById(groupId).get())
         .collect(Collectors.toList());
   }
 }
