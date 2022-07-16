5318a772fe24adf91af83ae318d4f53cdebe1a3a
Omar Yasser <oyaraouf@gmail.com>
2022-07-16 23:58:04 +0100

    Enable mongo auditing (#421)

---
+++ src/main/java/com/azkar/configs/MongoConfig.java
@@ -0,0 +1,11 @@
+package com.azkar.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.config.EnableReactiveMongoAuditing;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@Configuration
+@EnableMongoAuditing
+public class MongoConfig {}
