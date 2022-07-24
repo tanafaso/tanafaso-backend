998e69759595abe08700104af4f1c1e993e783b6
Omar Yasser <oyaraouf@gmail.com>
2022-07-24 01:16:50 +0100

    Ease getting started (#427)
    * Ease getting started

--- env-dev.sh
+++ env-dev.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
 
-export DATABASE_USER=springuser
-export DATABASE_NAME=azkar
-export DATABASE_PASSWORD=password
-export FACEBOOK_CLIENT_ID=example-facebook-client-id
-export FACEBOOK_CLIENT_SECRET=example-facebook-secret
-export GOOGLE_CLIENT_ID=example-google-client-id
-export GOOGLE_CLIENT_SECRET=example-google-client-secret
-export SERVER_HOST=localhost
+export DATABASE_URI=mongodb://tanafaso-db-username:tanafaso-db-password@mongo:27017/tanafaso
 export SERVER_PORT=8080
-export MONGODB_URI=localhost:27017/test
-export JWT_SECRET=JWT_SECRET
+export JWT_SECRET=example-secret
 export LOGS_PATH=./logs
 export MAIL_HOST=smtp.gmail.com
 export MAIL_USERNAME=example@example.com
-export MAIL_PASSWORD=example-password
+export MAIL_PASSWORD=example-mail-password
 export MAIL_PORT=587
-export APPLE_TEAM_ID=EXAMPLE_APPLE_TEAM_ID
-export APPLE_SIGN_IN_KEY_ID=EXAMPLE_ID
-
+export APPLE_TEAM_ID=example-apple-team-id
+export APPLE_SIGN_IN_KEY_ID=example-apple-sign-in-key
