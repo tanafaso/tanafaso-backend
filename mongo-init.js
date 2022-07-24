135ecc921b9320187656a3e9839e3ead6265c96d
Omar Yasser <oyaraouf@gmail.com>
2022-07-24 01:44:06 +0100

    Update mongo-init.js

--- mongo-init.js
+++ mongo-init.js
@@ -1,12 +1,2 @@
-db.createUser(
-        {
-            user: "tanafaso-db-username",
-            pwd: "tanafaso-db-password",
-            roles: [
-                {
-                    role: "readWrite",
-                    db: "tanafaso"
-                }
-            ]
-        }
-);
+db.createUser({user: "tanafaso-db-username", pwd: "tanafaso-db-password", roles: [ "readWrite", "dbAdmin" ]});
+db.grantRolesToUser('tanafaso-db-username', [{role: "readWrite", db: 'tanafaso' }]);
