8865da2cf631e6ba8822a035011fbf021ba43188
Omar Yasser <oyaraouf@gmail.com>
2023-03-26 13:08:58 +0100

    Add logging response interceptor

---
+++ src/main/java/com/azkar/configs/ResponseLoggingInterceptor.java
@@ -0,0 +1,35 @@
+package com.azkar.configs;
+
+
+import com.azkar.payload.ResponseBase;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+@ControllerAdvice
+public class ResponseLoggingInterceptor implements ResponseBodyAdvice<Object> {
+
+  private static final Logger logger = LoggerFactory.getLogger(ResponseLoggingInterceptor.class);
+
+  @Override
+  public boolean supports(MethodParameter returnType,
+      Class<? extends HttpMessageConverter<?>> converterType) {
+    return true;
+  }
+
+  @Override
+  public Object beforeBodyWrite(Object body, MethodParameter returnType,
+      MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType,
+      ServerHttpRequest request, ServerHttpResponse response) {
+    if (body instanceof ResponseBase) {
+      logger.info("The status code is {}", ((ResponseBase)body).getStatus().getCode());
+    }
+    return body;
+  }
+}
