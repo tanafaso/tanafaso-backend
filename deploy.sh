2f339ec478ea885ccadfd432a791a63ccebbddb1
Omar Yasser <oyaraouf@gmail.com>
2024-04-04 22:13:36 +0100

    Complete the deployment script

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,15 @@ set -e
 
 docker build --platform linux/amd64 -t tanafaso .
 
-docker tag tanafaso europe-west1-docker.pkg.dev/tanafaso/tanafaso/tanafaso
+random_tag=$(printf "randomtag%08d" $((RANDOM%100000000)))
 
-docker push europe-west1-docker.pkg.dev/tanafaso/tanafaso/tanafaso:latest
+docker tag tanafaso europe-west1-docker.pkg.dev/tanafaso/tanafaso/tanafaso:$random_tag
+
+docker push europe-west1-docker.pkg.dev/tanafaso/tanafaso/tanafaso:$random_tag
+
+sed -i '' "s|image: europe-west1-docker.pkg.dev/tanafaso/tanafaso/tanafaso:.*|image: europe-west1-docker.pkg.dev/tanafaso/tanafaso/tanafaso:$random_tag|" tanafaso-cloud-run-service.yaml
+
+gcloud run services replace tanafaso-cloud-run-service.yaml --region europe-west1
+
+echo "Deployed with tag: $random_tag"
 
